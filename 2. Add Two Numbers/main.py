906f1f2ba0adbefc44604a247be62adae541187c
LEE JESEOK <junelle@koreatech.ac.kr>
2021-07-06 04:56:46 +0900

    addTwoNumbers

--- 2. Add Two Numbers/main.py
+++ 2. Add Two Numbers/main.py
@@ -15,7 +15,45 @@ from typing import *
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        return
+        node1, node2 = l1, l2
+        
+        if node1 is None and node2 is None:
+            return
+        
+        result = None
+        node = result
+        carry = 0
+        while node1 or node2 or carry:
+            if node1 is not None:
+                val1 = node1.val
+            else:
+                val1 = 0
+            if node2 is not None:
+                val2 = node2.val
+            else:
+                val2 = 0
+            
+            val = val1 + val2 + carry
+            if val >= 10:
+                val -= 10
+                carry = 1
+            else:
+                carry = 0
+                
+            if result is None:
+                result = ListNode(val)
+                node = result
+            else:
+                valNode = ListNode(val)
+                node.next = valNode
+                node = valNode
+                
+            if node1 is not None:
+                node1 = node1.next
+            if node2 is not None:
+                node2 = node2.next
+            
+        return result
 
 l1 = [2,4,3], l2 = [5,6,4]
 solution = Solution()
