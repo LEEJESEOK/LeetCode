c805375e6dd6528b1ec54fc3e6de45eca1940513
LEE JESEOK <junelle@koreatech.ac.kr>
2021-07-06 05:13:14 +0900

    longestPalindrome

--- 5. Longest Palindromic Substring/main.py
+++ 5. Longest Palindromic Substring/main.py
@@ -9,19 +9,30 @@ import bisect
 from typing import *
 
 class Solution:
-
     def longestPalindrome(self, s: str) -> str:
-        if (s=='' or s == None):
-            return s
-
-        result=''
-        p=''
-        c=0        
-
-
-
-        return result
-
+        if s is None or s is '':
+            return
+        
+        start, end = 0, 0
+        
+        for i in range(len(s)):
+            len1 = self.expandAroundCenter(s, i, i)
+            len2 = self.expandAroundCenter(s, i, i + 1)
+            maxLen = max(len1, len2)
+            if maxLen > end - start:
+                start = i - (maxLen - 1) // 2
+                end = i + maxLen // 2
+        
+        return s[start:end+1]
+        
+    def expandAroundCenter(self, s: str, left: int, right: int) -> int:
+        L, R = left, right
+        while L >= 0 and R < len(s) and s[L] == s[R]:
+            L -= 1
+            R += 1
+            
+        return R - L - 1
+        
 
 # s = "babad"
 # s = "cbbd"
