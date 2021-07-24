5ca59db45db75ce1cfac144a83169dfd1ac9c9ac
LEE JESEOK <junelle@koreatech.ac.kr>
2021-07-24 17:34:51 +0900

    edit redundant loop

--- 104. Maximum Depth of Binary Tree/main.py
+++ 104. Maximum Depth of Binary Tree/main.py
@@ -22,38 +22,36 @@ from typing import *
 #         level = 0
 #         while root and nodes:
 #             level += 1
-            
+
 #             nodes = [
 #                 child for node in nodes for child
 #                 in {node.left, node.right} if child
 #             ]
 
 #         return level
+
+
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        if not root:
+        if root is None:
             return 0
-        queue = [root]
 
+        queue = collections.deque([root])
         level = 0
+
         while queue:
             level += 1
-            task=[]
-            queue.reverse()
-            i = 0
-            while queue:
-                task.append(queue.pop())
-                i += 1
-            for n in task:
-                if n:
-                    if n.left:
-                        queue.append(n.left)
-                    if n.right:
-                        queue.append(n.right)
+
+            for _ in range(len(queue)):
+                cur_root = queue.popleft()
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
 
         return level
 
 
-root = [3,9,20,None,None,15,7]
+root = [3, 9, 20, None, None, 15, 7]
 solution = Solution()
 print(solution.maxDepth(root))
